a0785ee77268fdd72435a8578fb071f8bb513272
Peter Stimpel <pstimpel+github@gmail.com>
2022-10-17 15:00:51 +0200

    Update DisableAllTwitterInterests.js

--- DisableAllTwitterInterests.js
+++ DisableAllTwitterInterests.js
@@ -4,7 +4,7 @@
 //found at https://www.neowin.net/guides/turn-off-topic-suggestions-and-interests-at-twitter-with-this-handy-script/
 //
 //
-//if you cannot paste, try "allow paste" first - feckin' pseudo security
+//if you cannot paste, try "allow paste" or "allow pasting" first - feckin' pseudo security https://stackoverflow.com/questions/26596858/how-to-disable-paste-protection-in-mozilla-firefox-developer-console
 
 var timer=1000; document.querySelectorAll( "div > input[type='checkbox']:checked" ).forEach((interest) => { setTimeout( function(){interest.click(); interest.scrollIntoView()},timer ); timer+=2000; });
 
